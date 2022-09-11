9c30e970e42fae3877a462ba68e02c4845d841f3
panqiuyu <1156822095@qq.com>
2022-09-11 11:51:37 +0800

    feat: update docs config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,6 +4,7 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "你好， VuePress ！",
   description: "这是我的第一个 VuePress 站点",
+  base:'/dmm-lib/',
   theme: defaultTheme({
     navbar: [
       { text: "bar1", link: "/guide/" },
