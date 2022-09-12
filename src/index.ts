36b60512f051c3bb986237865e4add163fce6652
panqiuyu <1156822095@qq.com>
2022-09-12 22:13:35 +0800

    feat: 2

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import Nftscan from '@/api/nftscan';
 import formatBigNumber from './util/number.util';
-import packageJson = require('./../package.json');
 
-const { version } = packageJson;
-
-export { Nftscan, formatBigNumber, version };
+export { Nftscan, formatBigNumber };
