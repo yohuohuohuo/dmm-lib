f8575bc01dc36af64f111530625b10638b9f5285
panqiuyu <1156822095@qq.com>
2022-09-13 17:55:29 +0800

    feat: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,28 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable import/no-extraneous-dependencies */
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
-
-const pkg = require('./package.json');
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: `src/index.ts`,
+  input: 'src/index.ts',
   output: [
-    { file: pkg.main, name: 'DmmLib', format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { dir: 'dist/umd', name: 'DmmLib', format: 'umd', sourcemap: true },
+    {
+      dir: 'dist/cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      dir: 'dist/esm',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      dir: 'dist/es',
+      format: 'es',
+      sourcemap: true,
+    },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
@@ -29,7 +39,7 @@ export default {
     // Allow node_modules resolution, so you can use 'external' to control
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+    nodeResolve(),
 
     // Resolve source maps to the original source
     sourceMaps(),
